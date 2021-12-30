97fa970104aed8d4599876db54ab6059ea374512
Cristian Pirnog <pirnog@gmail.com>
2021-12-30 12:18:58 +0100

    Improvements

--- vocabulary_tester.py
+++ vocabulary_tester.py
@@ -1,3 +1,4 @@
+import click
 from operator import itemgetter
 from pathlib import Path
 from typing import List
@@ -8,15 +9,14 @@ import random
 
 def loop(words, langs, max_count, random_language: bool = True):
     words = np.array(words)
-    count = 0
     mistakes = []
-    for k in np.random.choice(len(words), size=max_count, replace=False):
+    for i, k in enumerate(np.random.choice(len(words), size=max_count, replace=False)):
         idx = random.randint(0, 1) if random_language else 0
         other_idx = abs(idx - 1)
         word = words[k, idx]
 
         while True:
-            answer = input(f'[{langs[k][idx]}] {word} => [{langs[k][other_idx]}] ')
+            answer = input(f'({i+1:3d}/{max_count}). [{langs[k][idx]}] {word} => [{langs[k][other_idx]}] ')
             if len(answer) == 0:
                 print(f'\tYou must enter something...')
             else:
@@ -26,26 +26,35 @@ def loop(words, langs, max_count, random_language: bool = True):
         answer = answer.strip()
         if answer != expected:
             if answer.lower() == expected.lower():
-                print(f'Watch out for capitalisation. Correct was {expected}')
-                count += 1
+                print(f'\tWatch out for capitalisation. Correct was "{expected}"')
+            elif answer.replace(' ', '') == expected.replace(' ', ''):
+                print(f'\tWatch out for extra spaces in the answer')
             else:
                 mistakes.append((word, answer, expected, idx, other_idx))
                 print(f'\tWrong! Correct was "{expected}"')
-        else:
-            count += 1
 
-    return count, mistakes
+    return mistakes
+
+
+def process_words(words: str):
+    words = words.strip()
+    for k, v in  {'’': "'",}.items():
+        words = words.replace(k, v)
+    return words
 
 def read_words_file(file_name):
     words = []
     with file_name.open('r') as fr:
         for line in fr:
-            words.append(tuple(map(lambda x: x.strip(), line.replace('\n', '').split(';'))))
+            words.append(tuple(map(process_words, line.replace('\n', '').split(';'))))
     return words
 
-def run():
-    mistakes_file = 'mistakes.txt'
-    words = read_words_file(Path('words.txt'))
+@click.command()
+@click.argument('who', type=str)
+def run(who):
+    base_dir = Path(__file__).parent / who
+    words = read_words_file(base_dir / 'words.txt')
+    mistakes_file = base_dir / 'mistakes.txt'
     try:
         # Add previous mistakes three more times
         extra_words = read_words_file(Path(mistakes_file))
@@ -61,20 +70,20 @@ def run():
     langs = ('Fr', 'De')
     max_count = len(words) if input(f'There are {len(words)} words. Would you like to test all? [y/n] ').lower() == 'y' \
         else min(100, len(words))
-    count, mistakes = loop(words, [langs] * len(words), max_count)
+    mistakes = loop(words, [langs] * len(words), max_count)
 
     print("\n\n")
     if len(mistakes) == 0:
         print(f"Congratulations!!! You got everything right. Keep it up.")
         return
 
-    perc = int(float(count) / max_count * 100)
+    perc = int(1. - float(len(mistakes)) / max_count * 100)
     print(f"You got {perc}% right from {max_count} words with {len(mistakes)} mistakes.")
     print(f"Let's test only the words you got wrong?\n")
     idxs = select_tuple(mistakes, 3, 4)
     langs = [(langs[l[0]], langs[l[1]]) for l in idxs]
-    new_count, _ = loop(select_tuple(mistakes, 0, 2), langs, len(mistakes), random_language=False)
-    wrong_count = len(mistakes) - new_count
+    new_mistakes = loop(select_tuple(mistakes, 0, 2), langs, len(mistakes), random_language=False)
+    wrong_count = len(new_mistakes)
     print('')
     if wrong_count == 0:
         print(f'Very cool! Now you got them all right.')
@@ -94,7 +103,6 @@ def run():
 
 def select_tuple(input_: List[tuple], *args):
     return [tuple(itemgetter(*args)(i)) for i in input_]
-
 
 
 if __name__ == '__main__':
